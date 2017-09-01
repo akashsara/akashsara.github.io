34f2a5bfa3810b5c9afdc9d44c8f8230d0e7fa3c
Akash Saravanan <akashsara@outlook.com>
2017-09-02 00:49:49 +0530

    Added content. Resume icon. Changed background.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+    window.sr = ScrollReveal();
 
+    sr.reveal('.scrollFadeIn', {
+        duration: 1500,
+        origin:'top',
+        easing:'ease',
+        move: 0,
+    });
+
+    sr.reveal('.fadeInBox', {
+        duration: 1000,
+     }, 50);
 });
